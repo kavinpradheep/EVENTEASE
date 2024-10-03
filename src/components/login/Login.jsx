c78b8a2646bb16ac36df9f588980e9f57713065a
Dinesh7404 <dhanadinesh2004@gmail.com>
2024-10-03 21:47:07 +0530

    naga thanda

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import './login.css'
+import React from 'react';
+import './login.css';
 
 const Login = () => {
   return (
     <div className='login-page'>
-        
-        <div className="left-photo">
-            <img src="" alt="" />
+         <div className="left-photo">
+            {/* Use relative path for images in the public folder */}
+            <img src="/asserts/loginSvg.png" alt="Login illustration"  height={638} width={530}/>
         </div>
+        
         <div className="right-section">
             <h1>Create an Account</h1>
             <p>Already have an account? <span>Log in</span></p>
             <div className="name">
-                <input type='text' className="first-name" placeholder='First name'/>
-                <input type='text' className="last-name" placeholder='Last name' />
+                <input type='text' className="first-name" placeholder='First name' aria-label="First name"/>
+                <input type='text' className="last-name" placeholder='Last name' aria-label="Last name"/>
             </div>
 
-            <input type="mail" className='email' placeholder='Email'/>
-            <input type="password" className='password'  placeholder='Enter your password'/>
+            <input type="email" className='email' placeholder='Email' aria-label="Email"/>
+            <input type="password" className='password' placeholder='Enter your password' aria-label="Password"/>
             <button className="create-account">
-                Create accoount
+                Create account
             </button>
             <div className="or-register">
                 <hr />
                 <p>Or register with</p>
                 <hr />
             </div>
-            
         </div>
     </div>
   )
 }
 
-export default Login
+export default Login;
