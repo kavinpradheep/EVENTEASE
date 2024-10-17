717cb604574482f3808c0c47b5d1ad3ab255c6db
NKbharath <bharathnk2004@gmail.com>
2024-10-17 21:23:39 +0530

    pull route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-<<<<<<< HEAD
 import Adminhall from './components/adminhall/adminhall';
 import AdminLogin from './components/adminlogin/adminlogin';
-=======
-import LoginForm from './components/Actuallogin/LoginForm';
->>>>>>> 7a22d3db5875e1d9ea6c4c4e033f031344ce6349
 import Events from './components/allevents/allevents';
 import Login from './components/login/Login';
 import Mainpage from './components/homepage/home';
@@ -21,7 +17,6 @@ const App = () => {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
 
         <Route path='/Login-signup-page' element={<Login />} />
         <Route path='/Adminlogin' element={<AdminLogin />} />
@@ -34,13 +29,6 @@ const App = () => {
         <Route path='/aboutUs' element={<About/ >}/>
         <Route path='/Contact us' element={<Contact/ >} />
         
-=======
-        <Route path='/' element={<Login/>}/>
-        <Route path='/Actuallogin' element={<LoginForm/>}/>
-        <Route path='/Mainpage' element={<Mainpage/>}/>
-        <Route path='/Events' element={<Events/>}/>
-        <Route path='/EventRegister' element={<Eventregister/>} />
->>>>>>> 7a22d3db5875e1d9ea6c4c4e033f031344ce6349
       </Routes>
     </Router>
   );
